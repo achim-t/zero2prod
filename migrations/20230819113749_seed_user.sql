20fb18942fcc38bde0b4009312d4d9ac2187278b
achim-t <achim.t@gmail.com>
2023-08-19 14:13:25 +0200

    change password form

---
+++ migrations/20230819113749_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    'b96c9d1c-81ff-43a3-bf63-a131ff5f04ae',
+    'admin',
+    '$argon2id$v=19$m=4096,t=3,p=1$AjgEjSLLKdWlgfsTslByjw$vpa562Bi2xc1me0uvYSUH5WWPWbIICb5Ar9sdJCbDr0'
+)
